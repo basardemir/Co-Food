3fa9b8add170043f9aa911520c96cbc1e711da8a
Başar Demir <45140804+basardemir@users.noreply.github.com>
2020-12-08 00:40:35 +0300

    commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import psycopg2 as dbapi2
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("main_page.html")
+
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/restaurants')
+def restaurants():
+    return render_template("restaurants.html")
+
+@app.route('/restaurant')
+def restaurant():
+    return render_template("restaurant.html")
+if __name__ == '__main__':
+    app.run()
---
+++ movie.py
@@ -0,0 +1,3 @@
+class Movie:
+    def __init__(self):
+        self.movie = 1231
